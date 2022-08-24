2964ebe47346b7e13f510d1411d5512f6fc62aea
Nicholas Stewart <stewartnick53@icloud.com>
2022-08-23 21:07:31 -0500

    Added methods for getting all spies and adding new spies

--- Repositories/SpyRepository.cs
+++ Repositories/SpyRepository.cs
@@ -1,6 +1,19 @@
-﻿namespace SpyDuhCatWoman.Repositories
+﻿using SpyDuhCatWoman.Models;
+
+namespace SpyDuhCatWoman.Repositories
 {
     public class SpyRepository
     {
+        private static List<Spy> _spyData = new();
+
+        public void CreateSpy(Spy newSpy)
+        {
+            _spyData.Add(newSpy);
+        }
+
+        public List<Spy> GetAll()
+        {
+            return _spyData;
+        }
     }
 }
